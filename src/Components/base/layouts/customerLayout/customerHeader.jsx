969d5ed86beb78b61ccdda52c8bd8ca67c3512ca
mina ghoreshi <goreishi45@gmail.com>
2023-12-05 16:40:34 +0330

    add customer header

--- src/Components/base/layouts/customerLayout/customerHeader.jsx
+++ src/Components/base/layouts/customerLayout/customerHeader.jsx
@@ -1,5 +1,30 @@
 import React from "react";
-
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 export const CustomerHeader = () => {
-  return <div>customer header</div>;
+  let navigate = useNavigate();
+  return (
+    <header className="flex px-9 py-5 bg-primary text-white items-center justify-between">
+      <h1 className=" font-bold text-[60px] ">کت لند</h1>
+      <div className="flex gap-8 justify-self-end text-2xl">
+        <button
+          onClick={() => {
+            navigate("/admin-login");
+          }}
+        >
+          {" "}
+          مدیریت
+        </button>
+        <button
+          onClick={() => {
+            navigate("/cart");
+          }}
+          className="flex items-center gap-2"
+        >
+          {" "}
+          <Icon icon="mdi:cart-outline" color="white" /> سبد خرید
+        </button>
+      </div>
+    </header>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: `#211e45`,
+        secondary: `#e4e4f0`,
+      },
+      fontFamily: { custom: "farsi" },
+    },
   },
   plugins: [],
 };
