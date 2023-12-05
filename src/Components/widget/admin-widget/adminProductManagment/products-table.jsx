2ede28eb6cd0338c685b8bef6f1e5a2a37a505bd
mina ghoreshi <goreishi45@gmail.com>
2023-12-05 13:19:21 +0330

    feat: add data table base structure

--- src/Components/widget/admin-widget/adminProductManagment/products-table.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ProductsTable = () => {
-  return <div>this should be a table</div>;
-};
--- src/Components/widget/admin-widget/index.js
+++ src/Components/widget/admin-widget/index.js
@@ -1,4 +1,4 @@
-export * from "./adminProductManagment";
+export * from "./products-table";
 export * from "./adminStock";
 export * from "./adminOrders";
 export * from "./bottom-navigation";
