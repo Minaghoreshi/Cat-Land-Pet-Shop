decab606b787a3c1847df6955b5f6ce712586e8f
mina ghoreshi <goreishi45@gmail.com>
2023-12-05 11:36:42 +0330

    feat: add admin layout

--- src/Components/base/layouts/adminLayout/adminHeader.jsx
+++ src/Components/base/layouts/adminLayout/adminHeader.jsx
@@ -1,5 +1,25 @@
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 import React from "react";
+import { BottomNav } from "../../../widget/admin-widget";
 
 export const AdminHeader = () => {
-  return <div>AdmintestHeader</div>;
+  return (
+    <AppBar position="fixed">
+      <Toolbar sx={{ justifyContent: "space-between", padding: 2 }}>
+        <Box>
+          {" "}
+          <Typography variant="h1" color={"white"}>
+            پنل مدیریت کت لند
+          </Typography>
+        </Box>
+        <Box display={"flex"} gap={10}>
+          {" "}
+          <BottomNav />
+          <Button sx={{ color: "primary.x" }} href="/cart" variant="text">
+            بازگشت به سایت
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
---
+++ src/Components/widget/admin-widget/bottom-navigation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Divider,
+  Box,
+} from "@mui/material";
+export const BottomNav = () => {
+  const [value, setValue] = React.useState(0);
+  return (
+    <Box>
+      {" "}
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          console.log(newValue);
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="کالاها" />
+        <Divider orientation="vertical" flexItem />
+        <BottomNavigationAction label="موجودی و قیمت" />
+        <Divider orientation="vertical" flexItem />
+        <BottomNavigationAction label="سفارش ها" />
+      </BottomNavigation>
+    </Box>
+  );
+};
