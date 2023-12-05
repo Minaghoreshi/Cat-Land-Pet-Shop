479304387446511f47c46bd93f5295dd7e09ff40
mina ghoreshi <goreishi45@gmail.com>
2023-12-06 00:54:08 +0330

    feat: add admin products table

---
+++ src/Components/base/table.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Table = ({ data, columns }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.key} className="table--th">
+              {column.label}
+            </th>
+          ))}
+          {/* <th className="py-4 px-4 border border-gray-300 font-semibold text-right"></th> */}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex} className={"bg-gray-50"}>
+            {columns.map((column) => (
+              <td key={column.key} className="table-td ">
+                {row[column.key]}
+              </td>
+            ))}
+            <td className="py-4 px-4 border border-gray-300">
+              <button className="text-blue-500  hover:underline">ویرایش</button>{" "}
+              / <button className="text-red-500 hover:underline">حذف</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- src/pages/Admin/admin-products/AdminProducts.jsx
+++ src/pages/Admin/admin-products/AdminProducts.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 import { AdminLayout } from "../../../components";
+import Table from "../../../components/base/table";
 export const AdminProducts = () => {
-  return <AdminLayout>products table</AdminLayout>;
+  const data = [
+    { id: 1, image: "ندارد", name: "غذای خشک جوسرا سنسی کت", category: "غذا" },
+    { id: 2, image: "ندارد", name: "مالت پرسا", category: "وسایل جانبی" },
+    // ... more data
+  ];
+
+  const columns = [
+    { key: "image", label: "تصویر" },
+    { key: "name", label: "نام کالا" },
+    { key: "category", label: "دسته بندی" },
+  ];
+  return (
+    <AdminLayout>
+      <div className="mt-5 flex justify-between items-center w-3/4">
+        <span className="text-3xl ">مدیریت کالاها</span>
+        <button className="bg-save text-white rounded-md font-thin py-2 px-4 shadow-2xl">
+          افزودن کالا
+        </button>
+      </div>
+      <Table data={data} columns={columns} />
+    </AdminLayout>
+  );
 };
