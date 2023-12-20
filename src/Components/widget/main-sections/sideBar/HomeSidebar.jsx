deff660faaacf7e7f1ef1e730b72c0625c1a2841
mina ghoreshi <goreishi45@gmail.com>
2023-12-20 20:55:14 +0330

    feat:add products according to category to the state

--- src/Components/widget/main-sections/sideBar/HomeSidebar.jsx
+++ src/Components/widget/main-sections/sideBar/HomeSidebar.jsx
@@ -4,45 +4,7 @@ import { useQuery } from "react-query";
 import { getAllCategories } from "../../../../api/category/category-api";
 import { getSubCategoryByCategoryId } from "../../../../api/subcategory/subcategory-api";
 
-export const HomeSidebar = () => {
-  const [menuItems, setMenuItems] = useState([]);
-  const {
-    data: category,
-    error: categoryError,
-    isLoading: categoryLoading,
-  } = useQuery(["test"], getAllCategories);
-
-  useEffect(() => {
-    const fetchSubCategories = async () => {
-      if (category) {
-        const promises = category.map(async (category) => {
-          try {
-            const res = await getSubCategoryByCategoryId(category._id);
-            return {
-              name: category.name,
-              _id: category._id,
-              isOpen: false,
-              subItems: res,
-            };
-          } catch (error) {
-            console.log(error);
-            return {
-              name: category.name,
-              _id: category._id,
-              isOpen: false,
-              subItems: [],
-            };
-          }
-        });
-
-        const resolvedMenuItems = await Promise.all(promises);
-        setMenuItems(resolvedMenuItems);
-      }
-    };
-
-    fetchSubCategories();
-  }, [category]);
-
+export const HomeSidebar = ({ menuItems, setMenuItems }) => {
   const showSubCategory = (categoryId) => {
     setMenuItems((prevItems) => {
       return prevItems.map((item) =>
@@ -50,15 +12,6 @@ export const HomeSidebar = () => {
       );
     });
   };
-
-  if (categoryLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (categoryError) {
-    console.error("Error fetching data:", categoryError);
-    return <p>Error fetching data</p>;
-  }
 
   return (
     <div className="sidebar">
@@ -81,9 +34,9 @@ export const HomeSidebar = () => {
           <hr />
           {item.isOpen && (
             <ul className="sub-items">
-              {item.subItems.map((subItem, index) => (
-                <li key={subItem._id} className="sub-items-li">
-                  {subItem.name}
+              {item.subCategories.map((subcategory, index) => (
+                <li key={subcategory._id} className="sub-items-li">
+                  {subcategory.name}
                 </li>
               ))}
             </ul>
