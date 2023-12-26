601d3c24c2e29b9438cef29095ab34598864d492
mina ghoreshi <goreishi45@gmail.com>
2023-12-26 16:55:11 +0330

    feat: add formik and validation to checkout form

--- src/Components/widget/checkout-section/UserCheckout.jsx
+++ src/Components/widget/checkout-section/UserCheckout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { TableTitle } from "../tables";
-import { Label, TextInput, Textarea } from "flowbite-react";
+import { Button, Label, TextInput, Textarea } from "flowbite-react";
 import DatePicker from "react-multi-date-picker";
 import { Calendar } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
@@ -8,10 +8,13 @@ import persian_fa from "react-date-object/locales/persian_fa";
 import { useSelector } from "react-redux";
 import { useQuery } from "react-query";
 import { getUserById } from "../../../api/users/users-api";
+import { useFormik } from "formik";
+import { checkuotSchema } from "./checkout-schema";
+import { store } from "../../../store";
 export const UserCheckout = () => {
   const [value, setValue] = useState(new Date());
   const userId = useSelector((state) => state.user.userId);
-  console.log(userId);
+
   // const {
   //   data: user,
   //   isLoading,
@@ -28,13 +31,41 @@ export const UserCheckout = () => {
   // const handleDateChange = () => {
   //   console.log(value);
   // };
+
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      address: "",
+      phoneNumber: "",
+      deliveryDate: value,
+    },
+    validationSchema: checkuotSchema,
+    onSubmit: (values) => {
+      console.log(formik.values);
+      const userOrders = store.getState().user.userCart;
+      const updatedOrders = userOrders.map((order) => ({
+        ...order,
+        deliveryDate: values.deliveryDate,
+      }));
+
+      console.log(updatedOrders);
+    },
+  });
   useEffect(() => {
-    console.log(value?.toDate?.().toISOString());
+    // Set the value to a Date object
+    formik.setValues({
+      ...formik.values,
+      deliveryDate: value?.toDate?.().toISOString(),
+    });
   }, [value]);
   return (
     <div className="mt-5 flex  w-3/5 flex-col gap-10">
       <TableTitle title={"تکمیل اطلاعات کاربری"} />
-      <form className="shadow-custom rounded-3xl flex flex-col pt-5 py-10 px-24 gap-11 ">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="shadow-custom rounded-3xl flex flex-col items-center pt-5 py-10 px-24 gap-11 "
+      >
         <div className="flex gap-24 w-full justify-between ">
           <div className="w-5/12">
             <div className="mb-2 block">
@@ -44,7 +75,19 @@ export const UserCheckout = () => {
                 value="نام"
               />
             </div>
-            <TextInput id="firstname" type="text" className="w-full" required />{" "}
+            <TextInput
+              name="firstname"
+              id="firstname"
+              type="text"
+              className="w-full"
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.firstname}
+            />{" "}
+            {formik.touched.firstname && formik.errors.firstname ? (
+              <div className="text-red-500">{formik.errors.firstname}</div>
+            ) : null}
           </div>{" "}
           <div className="w-5/12">
             <div className="mb-2 block">
@@ -54,7 +97,19 @@ export const UserCheckout = () => {
                 value="نام خانوادگی"
               />
             </div>
-            <TextInput id="lastname" type="text" className="w-full" required />{" "}
+            <TextInput
+              name="lastname"
+              id="lastname"
+              type="text"
+              className="w-full"
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.lastname}
+            />{" "}
+            {formik.touched.lastname && formik.errors.lastname ? (
+              <div className="text-red-500">{formik.errors.lastname}</div>
+            ) : null}
           </div>
         </div>{" "}
         <div className="flex gap-24 w-full justify-between">
@@ -67,12 +122,19 @@ export const UserCheckout = () => {
               />
             </div>
             <Textarea
+              name="address"
               id="address"
               type="text"
               className="w-full resize-none"
               required
               rows={4}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.address}
             />{" "}
+            {formik.touched.address && formik.errors.address ? (
+              <div className="text-red-500">{formik.errors.address}</div>
+            ) : null}
           </div>{" "}
           <div className="w-5/12">
             <div className="mb-2 block">
@@ -83,11 +145,18 @@ export const UserCheckout = () => {
               />
             </div>
             <TextInput
+              name="phoneNumber"
               id="phoneNumber"
               type="text"
               className="w-full"
               required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.phoneNumber}
             />{" "}
+            {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+              <div className="text-red-500">{formik.errors.phoneNumber}</div>
+            ) : null}
           </div>
         </div>
         <div className="flex w-full ">
@@ -101,21 +170,31 @@ export const UserCheckout = () => {
               />
             </div>
             <DatePicker
-              value={value}
+              // name="deliveryDate"
+              // onChange={setValue}
+              // onBlur={formik.handleBlur}
+              // value={formik.values.deliveryDate}
+              // style={{ width: "200px", height: "50px" }}
+              // format="YYYY/MM/DD"
+              // calendar={persian}
+              // locale={persian_fa}
+              name="deliveryDate"
               onChange={setValue}
+              onBlur={formik.handleBlur}
+              value={formik.values.deliveryDate}
               style={{ width: "200px", height: "50px" }}
               format="YYYY/MM/DD"
               calendar={persian}
               locale={persian_fa}
             />
-            {/* <TextInput
-              id="deliveryDate"
-              type="date"
-              className="w-full resize-none"
-              required
-            />{" "} */}
           </div>{" "}
+          {formik.touched.deliveryDate && formik.errors.deliveryDate ? (
+            <div className="text-red-500">{formik.errors.deliveryDate}</div>
+          ) : null}
         </div>
+        <Button className="w-1/2" type="submit">
+          ادامه
+        </Button>
       </form>
     </div>
   );
---
+++ src/Components/widget/checkout-section/checkout-schema.js
@@ -0,0 +1,7 @@
+import * as Yup from "yup";
+export const checkuotSchema = Yup.object().shape({
+  firstname: Yup.string().required("نام الزامی است"),
+  lastname: Yup.string().required("نام خانوادگی الزامی است"),
+  address: Yup.string().required(" آدرس الزامی است"),
+  phoneNumber: Yup.number().required(" تلفن همراه الزامی است"),
+});
