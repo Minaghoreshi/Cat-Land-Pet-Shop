ac35c97ca776c422bbaef840d349ad623584a179
mina ghoreshi <goreishi45@gmail.com>
2024-01-04 02:13:11 +0330

    feat: add product details new design

--- src/Components/widget/product-detail-section/ProductDetails.jsx
+++ src/Components/widget/product-detail-section/ProductDetails.jsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 import { getProductById } from "../../../api/products/products-api";
 import "swiper/css";
 import * as DOMPurify from "dompurify";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -34,36 +33,38 @@ export const ProductDetails = () => {
   }, [data, product]);
 
   return product ? (
-    <div className="w-screen-3xl py-12 px-28 flex flex-col gap-20 ">
-      {" "}
-      <div className="flex gap-14">
+    <div className="w-full h-screen py-12 bg-secondary flex  justify-center gap-20 ">
+      <div className="bg-white w-2/3 p-8 rounded-lg flex flex-col gap-20  shadow-custom">
         {" "}
-        <CustomSwiper images={product.images} />
-        <ProductDescript
-          product={product}
-          name={product.name}
-          category={product.category.name}
-          subcategory={product.subcategory.name}
-          price={`${product.price.toLocaleString("en-US")}`}
-          quantity={product.quantity}
-        />
-        {product.quantity === 0 ? (
-          <div className="flex flex-col justify-center">
-            <span className="text-3xl text-selected">
-              در حال حاضر این محصول موجود نیست
-            </span>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-      <div className="flex flex-col gap-7">
-        <h2 className="text-primary text-3xl ">توضیحات محصول</h2>
-        <div
-          className="text-gray-500"
-          ref={myRef}
-          dangerouslySetInnerHTML={{ __html: myRef.current?.innerHTML || "" }}
-        ></div>
+        <div className="flex gap-14">
+          {" "}
+          <CustomSwiper images={product.images} />
+          <ProductDescript
+            product={product}
+            name={product.name}
+            category={product.category.name}
+            subcategory={product.subcategory.name}
+            price={`${product.price.toLocaleString("en-US")}`}
+            quantity={product.quantity}
+          />
+          {product.quantity === 0 ? (
+            <div className="flex flex-col justify-center">
+              <span className="text-3xl text-selected">
+                در حال حاضر این محصول موجود نیست
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="flex flex-col gap-7">
+          <h2 className="text-primary text-3xl ">توضیحات محصول</h2>
+          <div
+            className="text-gray-500"
+            ref={myRef}
+            dangerouslySetInnerHTML={{ __html: myRef.current?.innerHTML || "" }}
+          ></div>
+        </div>
       </div>
     </div>
   ) : (
